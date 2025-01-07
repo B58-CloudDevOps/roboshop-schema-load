b82193582c1d11e8da04b5987551d35cc21423bd
CodingManoj <CodingManoj@gmail.com>
2025-01-06 23:42:18 -0500

    Added Helm Hooks To Control The Flow

---
+++ run.sh
@@ -0,0 +1,23 @@
+if [ -z "$DB_TYPE" ]; then
+  echo DB_TYPE Input is missing
+  exit 1
+fi
+
+mkdir /app
+cd /app
+git clone $APP_GIT_URL .
+
+if [ "${DB_TYPE}" == "mongo" ]; then
+  for file in $SCHEMA_FILE ; do
+    echo "Loading File - $file"
+    mongosh --host $DB_HOST </app/db/$file.js
+  done
+
+fi
+
+if [ "${DB_TYPE}" == "mysql" ]; then
+  for file in $SCHEMA_FILE ; do
+    echo "Loading File - $file"
+    mysql -h $DB_HOST -u${DB_USER} -p${DB_PASS} < /app/db/$file.sql
+  done
+fi
